c5a28c2b9795641dcd233068165504a783885b92
flyingicedragon <1229760716@qq.com>
2020-11-03 16:18:03 +0800

    添加脚本和数据

---
+++ GO.R
@@ -0,0 +1,47 @@
+library(tidyverse)
+library(clusterProfiler)
+
+go_enricher <- function(gene_list, sample_name) {
+  ## import annotation data
+  go_annotation <- read_csv("tomato_go.csv")
+  go_annotation <- split(go_annotation, with(go_annotation, level))
+  go_info <- read_tsv("go-basic.tb")
+  type_vector <- c(
+    "biological_process",
+    "cellular_component",
+    "molecular_function"
+  )
+  ## GO analysis
+  go_split_enricher <- function(type) {
+    go_result_split <- enricher(
+      gene_list[, 1],
+      pAdjustMethod = "none",
+      TERM2GENE = go_annotation[[type]][c(2, 1)],
+      TERM2NAME = go_info[1:2]
+    )
+    return(go_result_split)
+  }
+  go_result <- list()
+  for (ii in type_vector) {
+    go_result[[ii]] <- go_split_enricher(ii)
+  }
+  ## dot plot
+  dotplot_save <- function(go_result, go_type) {
+    img <- dotplot(go_result, showCategory = 30)
+    if (go_type == "molecular_function") {
+      img_width = 12
+    } else {
+      img_width = 8
+    }
+    ggsave(
+      paste0(sample_name, "_", go_type, ".pdf"),
+      device = pdf(),
+      plot = img,
+      width = img_width,
+      height = 6
+    )
+  }
+  for (ii in type_vector) {
+    dotplot_save(go_result[[ii]], ii)
+  }
+}
