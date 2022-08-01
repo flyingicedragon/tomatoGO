9e1f3aca8ec723397461b0a48ab0035b9bf2a465
icedragon <flyingicedragon@qq.com>
2022-08-01 22:09:42 +0800

    revigo
    Signed-off-by: icedragon <flyingicedragon@qq.com>

--- GO.R
+++ GO.R
@@ -132,3 +132,108 @@ get_go_genes <- function(gene_list, example_name) {
     get_genes()
   return(gene_go)
 }
+
+#' @title get GO results
+#' @param genes vector contains genes
+#' @param value value names for export. eg. pvalue, p.adjust, qvalue, Count
+#' @return tibble containing results
+get_go_results <- function(genes, value = "pvalue") {
+  go_result <- enricher(
+    genes,
+    pAdjustMethod = "none",
+    TERM2GENE = go_annotation_all[c(2, 1)],
+    TERM2NAME = go_info[1:2]
+  ) %>%
+    tibble::as_tibble() %>%
+    select("ID", !!value)
+  go_result
+}
+
+# revigo -----
+
+#' @title Export GO result with p-value
+#'
+#' @param genes genes list
+export_go_results <- function(genes, path) {
+  result <- get_go_results(genes[["name"]])
+  write_tsv(
+    result,
+    path,
+    col.names = FALSE
+  )
+  shell_str <- paste0(
+    "java -jar /mnt/hdd0/icedragon/packages/",
+    "RevigoStandalone_2015-02-17_beta/RevigoStandalone.jar ",
+    path, " --taxId=4081"
+  )
+  print(shell_str)
+}
+
+#' @title Read revigo table
+#'
+#' @return revigo tibble
+read_revigo <- function(path) {
+  result <- read_tsv(path) %>%
+    dplyr::filter(eliminated == 0) %>%
+    mutate(plot_X = as.numeric(plot_X), plot_Y = as.numeric(plot_Y))
+  result
+}
+
+#' @title Plot revigo results
+#'
+#' @param result filtered result of revigo
+plot_revigo_scatter <- function(result) {
+  pic <- list()
+  for (item in c(
+    "Biological_Process", "Cellular_Component", "Molecular_Function"
+  )) {
+    result_one <- dplyr::filter(result, `%namespace` == item)
+    pic[[item]] <- plot_revigo_scatter_one(result_one)
+  }
+  pic
+}
+
+#' @title Plot revigo results per item
+#'
+#' @param result_one one result item
+#'
+#' @return ggplot2 object
+plot_revigo_scatter_one <- function(result_one) {
+  p1 <- ggplot(data = result_one)
+  p1 <- p1 + geom_point(aes(
+    plot_X, plot_Y,
+    size = plot_size, color = `log10_p-value`,
+    alpha = I(0.6)
+  ))
+  p1 <- p1 + scale_colour_gradientn(
+    colors = c("red", "yellow", "green"),
+    limits = c(min(result_one[["log10_p-value"]]), 0)
+  )
+  p1 <- p1 + scale_size(range = c(5, 30)) + theme_bw()
+  ex <- dplyr::filter(
+    result_one,
+    dispensability < 0.1 |
+      label == 1 # |
+    # `log10_p-value` < -6
+  )
+  p1 <- p1 + geom_text(
+    data = ex,
+    aes(plot_X, plot_Y, label = description),
+    color = I(alpha("black", 0.85)),
+    size = 5
+  )
+  p1 <- p1 + labs(y = "semantic space x", x = "semantic space y")
+  p1 <- p1 + theme(legend.key = element_blank())
+  p1 <- p1 + theme(text = element_text(size = 18))
+  result_x <- max(result_one$plot_X) - min(result_one$plot_X)
+  result_y <- max(result_one$plot_Y) - min(result_one$plot_Y)
+  p1 <- p1 + xlim(
+    min(result_one$plot_X) - result_x / 10,
+    max(result_one$plot_X) + result_x / 10
+  )
+  p1 <- p1 + ylim(
+    min(result_one$plot_Y) - result_y / 10,
+    max(result_one$plot_Y) + result_y / 10
+  )
+  p1
+}
