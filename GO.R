5cdfc2b31bdf68b12f8eae6f7d1515e613275b10
icedragon <flyingicedragon@qq.com>
2022-08-03 20:08:24 +0800

    重构

--- GO.R
+++ GO.R
@@ -23,7 +23,7 @@ type_vector <- c(
 
 # GO ----
 
-#' @title split GO enricher
+#' @title Split GO enricher
 #' @param genes vector to do GO analysis
 #' @param GO types
 #' @return splitted GO enricher
@@ -34,66 +34,95 @@ go_split_enricher <- function(gene_list, type = FALSE) {
     TERM2GENE = go_annotation[[type]][c(2, 1)],
     TERM2NAME = go_info[1:2]
   )
-  return(go_result_split)
+  go_result_split
 }
 
 #' @title GO analysis per type
 #' @param gene_list genes vector
 #' @param sample_name sample name used in result file
+#' @return GO results list
 go_enricher <- function(gene_list, sample_name) {
   go_result <- list()
-  for (ii in type_vector) {
-    go_result[[ii]] <- go_split_enricher(gene_list, ii)
+  for (type in type_vector) {
+    go_result[[type]] <- go_split_enricher(gene_list, type)
   }
-  barplot_save <- function(go_result, go_type) {
-    img <- barplot(go_result, showCategory = 30)
-    if (go_type == "molecular_function") {
-      img_width <- 12
-    } else {
-      img_width <- 8
-    }
-    ggsave(
-      paste0(sample_name, "_", go_type, ".pdf"),
-      device = pdf(),
-      plot = img,
-      width = img_width,
-      height = 6
-    )
-  }
-  for (ii in type_vector) {
-    barplot_save(go_result[[ii]], ii)
-  }
-  return(go_result)
+  go_result
 }
 
 #' @title GO analysis for all
 #' @param gene_list genes vector
-#' @param sample_name sample name used in result file
-go_enricher_all <- function(gene_list, sample_name) {
+#' @return GO result
+go_enricher_all <- function(gene_list) {
   go_result <- enricher(
     gene_list,
     pAdjustMethod = "none",
     TERM2GENE = go_annotation_all[c(2, 1)],
     TERM2NAME = go_info[1:2]
   )
-  ## bar plot
-  barplot_save <- function(go_result) {
-    img <- barplot(go_result, showCategory = 30)
+  go_result
+}
+
+#' @title Plot GO result
+#' @param go_result GO result
+#' @param ... other parameters for barplot
+#' @return img object
+plot_go <- function(go_result, ...) {
+  img <- barplot(go_result, showCategory = 30, ...)
+  img
+}
+
+#' @title Plot GO result for each GO type
+#' @param go_result GO results for each GO type
+#' @param ... other parameters for barplot
+#' @return imgs list
+plot_go_each <- function(go_results, ...) {
+  imgs <- list()
+  for (go_type in type_vector) {
+    imgs[go_type] <- plot_go(go_results[go_type], ...)
+  }
+  imgs
+}
+
+#' @title Save plot
+#' @param img plot object
+#' @param go_type GO type
+#' @param path path to save
+#' @param sample_name file name to save
+plot_save <- function(img, go_type = "all", path = ".", sample_name = "") {
+  if (go_type == "molecular_function") {
+    img_width <- 12
+  } else {
+    img_width <- 8
+  }
+  if (go_type == "all") {
+    sample_name <- paste0(sample_name, "_all.pdf")
+    sample_name <- file.path(path, sample_name)
     ggsave(
-      paste0(sample_name, ".pdf"),
+      sample_name,
+      device = pdf(),
       plot = img,
-      width = 12,
+      width = img_width,
       height = 6
     )
+  } else {
+    for (go_type in type_vector) {
+      sample_name_one <- paste0(sample_name, "_", go_type, ".pdf")
+      sample_name_one <- file.path(path, sample_name_one)
+      ggsave(
+        sample_name_one,
+        device = pdf(),
+        plot = img,
+        width = img_width,
+        height = 6
+      )
+    }
   }
-  barplot_save(go_result)
-  return(go_result)
 }
 
 #' @title get GO genes
-#' @param GO result
-#' @return GO result data
-get_genes <- function(go_result) {
+#' @param go_result GO result
+#' @return GO result data matrix
+get_genes <- function(go_result, all = FALSE) {
   type_vector <- c(
     "biological_process",
     "cellular_component",
@@ -120,7 +149,7 @@ get_genes <- function(go_result) {
     rbind(
       go_result_list[[3]] %>% as.matrix()
     )
-  return(go_result)
+  go_result
 }
 
 #' @title get GO genes
@@ -130,21 +159,15 @@ get_genes <- function(go_result) {
 get_go_genes <- function(gene_list, example_name) {
   gene_go <- go_enricher(gene_list, example_name) %>%
     get_genes()
-  return(gene_go)
+  gene_go
 }
 
 #' @title get GO results
-#' @param genes vector contains genes
+#' @param result GO result all
 #' @param value value names for export. eg. pvalue, p.adjust, qvalue, Count
 #' @return tibble containing results
-get_go_results <- function(genes, value = "pvalue") {
-  go_result <- enricher(
-    genes,
-    pAdjustMethod = "none",
-    TERM2GENE = go_annotation_all[c(2, 1)],
-    TERM2NAME = go_info[1:2]
-  ) %>%
-    tibble::as_tibble() %>%
+get_go_results <- function(result, value = "pvalue") {
+  go_result <- tibble::as_tibble(result) %>%
     select("ID", !!value)
   go_result
 }
@@ -152,14 +175,15 @@ get_go_results <- function(genes, value = "pvalue") {
 # revigo -----
 
 #' @title Export GO result with p-value
-#'
-#' @param genes genes list
-export_go_results <- function(genes, path) {
-  result <- get_go_results(genes[["name"]])
+#' @param genes genes vector
+#' @param ... other parameters for get_go_results
+export_go_results <- function(genes, path, ...) {
+  result <- go_enricher_all(genes) %>%
+    get_go_results(genes, ...)
   write_tsv(
     result,
     path,
-    col.names = FALSE
+    col_names = FALSE
   )
   shell_str <- paste0(
     "java -jar /mnt/hdd0/icedragon/packages/",
@@ -167,6 +191,7 @@ export_go_results <- function(genes, path) {
     path, " --taxId=4081"
   )
   print(shell_str)
+  shell_str
 }
 
 #' @title Read revigo table
@@ -182,13 +207,13 @@ read_revigo <- function(path) {
 #' @title Plot revigo results
 #'
 #' @param result filtered result of revigo
-plot_revigo_scatter <- function(result) {
+plot_revigo_scatter <- function(result, ...) {
   pic <- list()
   for (item in c(
     "Biological_Process", "Cellular_Component", "Molecular_Function"
   )) {
     result_one <- dplyr::filter(result, `%namespace` == item)
-    pic[[item]] <- plot_revigo_scatter_one(result_one)
+    pic[[item]] <- plot_revigo_scatter_one(result_one, ...)
   }
   pic
 }
@@ -196,9 +221,10 @@ plot_revigo_scatter <- function(result) {
 #' @title Plot revigo results per item
 #'
 #' @param result_one one result item
+#' @param condition label show condition
 #'
 #' @return ggplot2 object
-plot_revigo_scatter_one <- function(result_one) {
+plot_revigo_scatter_one <- function(result_one, top = FALSE, ...) {
   p1 <- ggplot(data = result_one)
   p1 <- p1 + geom_point(aes(
     plot_X, plot_Y,
@@ -206,16 +232,22 @@ plot_revigo_scatter_one <- function(result_one) {
     alpha = I(0.6)
   ))
   p1 <- p1 + scale_colour_gradientn(
-    colors = c("red", "yellow", "green"),
-    limits = c(min(result_one[["log10_p-value"]]), 0)
+    colors = c("red", "red", "yellow", "green"),
+    limits = c(min(result_one[["log10_p-value"]]), 0),
+    values = scales::rescale(c(min(result_one[["log10_p-value"]]), -10, -3, 0))
   )
   p1 <- p1 + scale_size(range = c(5, 30)) + theme_bw()
-  ex <- dplyr::filter(
-    result_one,
-    dispensability < 0.1 |
-      label == 1 # |
-    # `log10_p-value` < -6
-  )
+  if (top) {
+    ex <- top_n(result_one, top, dispensability)
+  } else {
+    ex <- dplyr::filter(
+      result_one,
+      ...
+      ## dispensability < 0.1 |
+      ##   label == 1 |
+      ##   `log10_p-value` < -6
+    )
+  }
   p1 <- p1 + geom_text(
     data = ex,
     aes(plot_X, plot_Y, label = description),
